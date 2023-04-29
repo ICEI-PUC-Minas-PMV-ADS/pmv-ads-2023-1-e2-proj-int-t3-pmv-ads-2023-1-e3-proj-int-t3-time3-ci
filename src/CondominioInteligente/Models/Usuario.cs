2317952afd7a24636ad8a951ae8002b9c01b1d67
NayaraMenezes <Nayaramenezes.dev@gmail.com>
2023-04-29 12:04:26 -0300

    Criação de projeto.

---
+++ src/CondominioInteligente/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CondominioInteligente.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Cod_Usuario { get; set; }
+        [Required(ErrorMessage = "Obrigatório Informar o Nome")]
+
+        public int Cod_Tipo_Usuario { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
