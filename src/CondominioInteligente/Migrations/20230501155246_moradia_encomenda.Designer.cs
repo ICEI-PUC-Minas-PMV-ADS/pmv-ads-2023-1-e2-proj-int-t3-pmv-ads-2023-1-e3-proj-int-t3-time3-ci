129d98f0745df339ac313934e9e310df4d27fb62
Talita Reis dos Santos Pereira <talitarsp@outlook.com>
2023-05-01 12:54:06 -0300

    criação dos migrations para os models moradia e encomenda

--- src/CondominioInteligente/Migrations/20230501155246_moradia_encomenda.Designer.cs
+++ src/CondominioInteligente/Migrations/20230501155246_moradia_encomenda.Designer.cs
@@ -4,6 +4,7 @@ using CondominioInteligente.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CondominioInteligente.Migrations
 {
     [DbContext(typeof(CondominioInteligenteContext))]
-    partial class CondominioInteligenteContextModelSnapshot : ModelSnapshot
+    [Migration("20230501155246_moradia_encomenda")]
+    partial class moradia_encomenda
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
