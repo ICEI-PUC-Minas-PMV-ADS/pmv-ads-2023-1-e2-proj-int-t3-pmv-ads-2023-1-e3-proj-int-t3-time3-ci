08427ec9ab3e6b449b8238eda5ac79026f9f2d80
Talita Reis dos Santos Pereira <talitarsp@outlook.com>
2023-06-18 16:51:28 -0300

    ajuste do login/logout

--- src/CondominioInteligente/Controllers/SolicitacaoAreaComumsController.cs
+++ src/CondominioInteligente/Controllers/SolicitacaoAreaComumsController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CondominioInteligente.Data;
 using CondominioInteligente.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CondominioInteligente.Controllers
 {
+    [Authorize]
     public class SolicitacaoAreaComumsController : Controller
     {
         private readonly CondominioInteligenteContext _context;
--- src/CondominioInteligente/Migrations/20230523013219_tabela_de_usuario.Designer.cs
+++ src/CondominioInteligente/Migrations/20230523013219_tabela_de_usuario.Designer.cs
@@ -4,6 +4,7 @@ using CondominioInteligente.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CondominioInteligente.Migrations
 {
     [DbContext(typeof(CondominioInteligenteContext))]
-    partial class CondominioInteligenteContextModelSnapshot : ModelSnapshot
+    [Migration("20230523013219_tabela_de_usuario")]
+    partial class tabela_de_usuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -200,9 +203,8 @@ namespace CondominioInteligente.Migrations
                     b.Property<int>("Perfil")
                         .HasColumnType("int");
 
-                    b.Property<string>("Senha")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<int>("Senha")
+                        .HasColumnType("int");
 
                     b.Property<string>("Telefone")
                         .IsRequired()
